06404a5436c73361b9893ff76c2eb2abf618e162
Dzeneta <dzeneta.kudumovic@tacta.io>
2024-02-25 16:57:45 +0100

    Initial setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(name='distfeatselect',
+      packages=['distfeatselect'],
+      version='0.1.0',
+      description='A Pyhton package implementing distributed feature selection algorithm.',
+      url='https://github.com/dkudumovic1/distfeatselect',
+      download_url='https://github.com/dkudumovic1/distfeatselect/archive/0.1.0.tar.gz',  # FILL IN LATER
+      author='Dzeneta Kudumovic',
+      author_email='dzeneta.kudum@gmail.com',
+      keywords=['feature selection', 'distributed feature selection', 'randomised feature selection'],
+      license='MIT',
+
+      install_requires=[
+          'numpy',
+          'pandas',
+          'statsmodels',
+          'scikit-learn',
+          'dcor',
+      ],
+
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+      ],
+      )
